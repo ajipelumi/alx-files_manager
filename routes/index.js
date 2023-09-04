3312fda620f8450df7dfb708c6d2084c1df95e01
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-09-04 19:36:53 +0100

    implemented first API

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import AppController from '../controllers/app.controller';
+import AppController from '../controllers/AppController';
 
 // Create router
 const router = express.Router();
