c83e130d3fddd3ce5c99e17c8d9e832db485f64b
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-09-04 14:54:23 +0100

    setup redis

--- utils/redis.js
+++ utils/redis.js
@@ -17,38 +17,38 @@ class RedisClient {
   }
 
   // Function to check if connection is a success
-    isAlive() {
-        // Return the connection status
-        return this.connect;
-    }
+  isAlive() {
+    // Return the connection status
+    return this.connect;
+  }
 
   // Function to get a key
-    async get(key) {
-        // Promisify the get function
-        const getAsync = promisify(this.client.get).bind(this.client);
-        // Get the value of the key 
-        const value = await getAsync(key);
-        // Return the value
-        return value;
-    }
+  async get(key) {
+    // Promisify the get function
+    const getAsync = promisify(this.client.get).bind(this.client);
+    // Get the value of the key
+    const value = await getAsync(key);
+    // Return the value
+    return value;
+  }
 
-    // Function to set a key
-    async set(key, value, duration) {
-        // Promisify the set function
-        const setAsync = promisify(this.client.set).bind(this.client);
-        // Set the value of the key
-        await setAsync(key, value);
-        // Set the expiration of the key
-        this.client.expire(key, duration);
-    }
+  // Function to set a key
+  async set(key, value, duration) {
+    // Promisify the set function
+    const setAsync = promisify(this.client.set).bind(this.client);
+    // Set the value of the key
+    await setAsync(key, value);
+    // Set the expiration of the key
+    this.client.expire(key, duration);
+  }
 
-    // Function to delete a key
-    async del(key) {
-        // Promisify the del function
-        const delAsync = promisify(this.client.del).bind(this.client);
-        // Delete the key
-        await delAsync(key);
-    }
+  // Function to delete a key
+  async del(key) {
+    // Promisify the del function
+    const delAsync = promisify(this.client.del).bind(this.client);
+    // Delete the key
+    await delAsync(key);
+  }
 }
 
 // Create an instance of Redis Client
